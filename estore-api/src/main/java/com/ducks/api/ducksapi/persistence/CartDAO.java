bdb6ea9635a56e365a0937c3d85fac7c3ac3217f
Le <ajl5531@rit.edu>
2023-02-25 00:59:47 -0500

    Added commenting with Javadoc formatting

--- estore-api/src/main/java/com/ducks/api/ducksapi/persistence/CartDAO.java
+++ estore-api/src/main/java/com/ducks/api/ducksapi/persistence/CartDAO.java
@@ -14,15 +14,74 @@ import com.ducks.api.ducksapi.model.ShoppingCart;
  */
 public interface CartDAO {
     
+    /**
+     * Retrieves all {@linkplain ShoppingCart shopping cart}
+     * 
+     * @return An array of {@link ShoppingCart shopping cart} objects, may be empty
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
     ShoppingCart[] getShoppingCarts() throws IOException;
 
+    /**
+     * Finds all {@linkplain ShoppingCart shopping cart} whose id contains the given text
+     * 
+     * @param containsText The text to match against
+     * 
+     * @return An array of {@link ShoppingCart shopping cart} whose id contains the given text, may be empty
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
     ShoppingCart[] findShoppingCarts(String containsText) throws IOException;
 
+    /**
+     * Retrieves a {@linkplain ShoppingCart shopping cart} with the given customer id
+     * 
+     * @param id The id of the {@link ShoppingCart shopping cart} to get
+     * 
+     * @return a {@link ShoppingCart shopping cart} object with the matching id
+     * <br>
+     * null if no {@link ShoppingCart shopping cart} with a matching id is found
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
     ShoppingCart getShoppingCart() throws IOException;
 
+    /**
+     * Creates and saves a {@linkplain ShoppingCart shopping cart}
+     * 
+     * @param cart {@linkplain ShoppingCart shopping cart} object to be created and saved
+     * <br>
+     * The id of the shopping cart object is ignored and a new uniqe id is assigned
+     *
+     * @return new {@link ShoppingCart shopping cart} if successful, false otherwise 
+     * 
+     * @throws IOException if an issue with underlying storage
+     */
     ShoppingCart createShoppingCart(ShoppingCart cart) throws IOException;
 
+    /**
+     * Updates and saves a {@linkplain ShoppingCart shopping cart}
+     * 
+     * @param cart {@link ShoppingCart shopping cart} object to be updated and saved
+     * 
+     * @return updated {@link ShoppingCart shopping cart} if successful, null if
+     * {@link ShoppingCart shopping cart} could not be found
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     */
     ShoppingCart updateShoppingCart(ShoppingCart cart) throws IOException;
 
-    boolean deleteShoppingCart() throws IOException;
+    /**
+     * Deletes a {@linkplain ShoppingCart shopping cart} with the given customer id
+     * 
+     * @param id The id of the {@link ShoppingCart shopping cart}
+     * 
+     * @return true if the {@link ShoppingCart shopping cart} was deleted
+     * <br>
+     * false if duck with the given id does not exist
+     * 
+     * @throws IOException if underlying storage cannot be accessed
+     */
+    boolean deleteShoppingCart(int id) throws IOException;
 }
