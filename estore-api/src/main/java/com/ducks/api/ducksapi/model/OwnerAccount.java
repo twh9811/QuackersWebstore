fafafa6b0a8b911b8ec59e115c480c0662a6c315
Travis <travishill62102@gmail.com>
2023-02-26 15:22:56 -0500

    got rid of certain account changes due to confusion prior to implementing

--- estore-api/src/main/java/com/ducks/api/ducksapi/model/OwnerAccount.java
+++ estore-api/src/main/java/com/ducks/api/ducksapi/model/OwnerAccount.java
@@ -1,20 +1,8 @@
 package com.ducks.api.ducksapi.model;
 
 public class OwnerAccount extends Account {
-    
+
     public OwnerAccount() {
         super();
-    }
-
-    public boolean addInventory() {
-        return true;
-    }
-
-    public boolean removeInventory() {
-        return true;
-    }
-
-    public boolean updateInventory() {
-        return true;
     }
 }
