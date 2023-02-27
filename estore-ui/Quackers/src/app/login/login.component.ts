3429020c71e85ccd40413a1b30922112423e74af
Travis <travishill62102@gmail.com>
2023-02-26 21:04:09 -0500

    Login page working

--- estore-ui/Quackers/src/app/login/login.component.ts
+++ estore-ui/Quackers/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Login } from '../login';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+
+  attempt : Login = {
+    username : "",
+    password : ""
+  };
+
+  onSubmit(username : String, password : String) {
+      this.attempt.username = username;
+      this.attempt.password = password;
+  }
+
 }
