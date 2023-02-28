5f99d7af5fa8c647591091868b2bdfffbf600829
twh9811 <69920367+twh9811@users.noreply.github.com>
2023-02-28 14:25:43 -0500

    Page displaying works much better

--- estore-ui/Quackers/src/app/app-routing.module.ts
+++ estore-ui/Quackers/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminTestComponent } from './admin-test/admin-test.component';
 import { CustomerTestComponent } from './customer-test/customer-test.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full"},
   { path: 'adminPage', component: AdminTestComponent },
-  { path: 'customerPage', component: CustomerTestComponent }
+  { path: 'customerPage', component: CustomerTestComponent },
+  { path: "login", component : LoginComponent}
 ];
 
 @NgModule({
