5083703d98c2d72f5272135903cbaaaae334a783
Travis <travishill62102@gmail.com>
2023-03-01 18:38:19 -0500

    Added logout button

--- estore-ui/Quackers/src/app/customer-test/customer-test.component.ts
+++ estore-ui/Quackers/src/app/customer-test/customer-test.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-test',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-test.component.css']
 })
 export class CustomerTestComponent {
+  constructor(private router : Router) {}
 
+  logout() {
+    this.router.navigate([''])
+  }
 }
