c54f9cc704c6872a9322726c8a54af175d2cbdf9
Travis <travishill62102@gmail.com>
2023-03-02 12:53:26 -0500

    Fixed OwnerAccount based on code review suggestions

--- estore-api/src/main/java/com/ducks/api/ducksapi/model/OwnerAccount.java
+++ estore-api/src/main/java/com/ducks/api/ducksapi/model/OwnerAccount.java
@@ -1,7 +1,17 @@
 package com.ducks.api.ducksapi.model;
 
+
+/**
+ * A Account class that represents an Admin on the webstore.
+ * 
+ * @author Travis Hill
+ */
+
 public class OwnerAccount extends Account {
 
+    /**
+     * Creates a new Admin account using the default values defined in the Account class.
+     */
     public OwnerAccount() {
         super();
     }
