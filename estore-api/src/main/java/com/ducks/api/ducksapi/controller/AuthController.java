69e70ed85bc9e59e20ceebb1ea621a84bdbc2f27
Travis <travishill62102@gmail.com>
2023-03-02 17:26:29 -0500

    getAccount implemented

--- estore-api/src/main/java/com/ducks/api/ducksapi/controller/AuthController.java
+++ estore-api/src/main/java/com/ducks/api/ducksapi/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.ducks.api.ducksapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ducks.api.ducksapi.model.Account;
 import com.ducks.api.ducksapi.persistence.AccountDAO;
 
 /**
@@ -29,5 +34,29 @@ public class AuthController {
     */
     public AuthController(AccountDAO accountDAO) {
         this.accountDAO = accountDAO;
+    }
+
+
+    /**
+     * Responds to the GET request for a ({@linkplain} Account account} for the specific ID
+     * This is needed to get valid data to compare to the potentially invalid user-input data
+     * 
+     * @param id the id of the account to be fetched
+     * @return the account object
+     * 
+     * ResponseEntity with HTTP status of OK if found
+     * ResponseEntity with HTTP status of NOT_FOUND if not found.
+     * ResponseEntity with HTTP status of Internal Server Error if anything else happens
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccount(@PathVariable int id) {
+        try {
+            Account account = accountDAO.getAccount(id);
+            if(account != null) {
+                return new ResponseEntity<Account>(account, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
     }
 }
