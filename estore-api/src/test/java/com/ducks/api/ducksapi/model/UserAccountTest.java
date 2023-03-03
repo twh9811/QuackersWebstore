a0c16e19effa100f91f180134bbfcdc062805d53
Le <ajl5531@rit.edu>
2023-03-03 14:15:32 -0500

    Implemented invoke for UserAccountTest

--- estore-api/src/test/java/com/ducks/api/ducksapi/model/UserAccountTest.java
+++ estore-api/src/test/java/com/ducks/api/ducksapi/model/UserAccountTest.java
@@ -2,6 +2,7 @@ package com.ducks.api.ducksapi.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.apache.catalina.User;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +19,13 @@ public class UserAccountTest {
         // Setup
         int expected_id = 11;
         String expected_username = "andyromede";
-        String expected_password = "123456";
+        String expected_plainPassword = "123456";
         String expected_hashedPassword = "654321";
         boolean expected_adminStatus = false;
-
         
+        //invoke
+        UserAccount account_one = new UserAccount(expected_id, expected_username, expected_plainPassword);
+
+        UserAccount account_two = new UserAccount(expected_id, expected_username, expected_hashedPassword);
     }
 }
