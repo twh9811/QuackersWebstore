37a3cf25ca8fdd8a1c2838fd4afe05272f9b3f20
Le <ajl5531@rit.edu>
2023-03-03 13:59:20 -0500

    implemented setup for UserAccountTest

--- estore-api/src/test/java/com/ducks/api/ducksapi/model/UserAccountTest.java
+++ estore-api/src/test/java/com/ducks/api/ducksapi/model/UserAccountTest.java
@@ -1,5 +1,27 @@
 package com.ducks.api.ducksapi.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The unit test suite for the UserAccount class
+ * 
+ * @author Andrew Le
+ */
+@Tag("Model-tier")
 public class UserAccountTest {
-    
+
+    @Test
+    public void testConstructor() {
+        // Setup
+        int expected_id = 11;
+        String expected_username = "andyromede";
+        String expected_password = "123456";
+        String expected_hashedPassword = "654321";
+        boolean expected_adminStatus = false;
+
+        
+    }
 }
