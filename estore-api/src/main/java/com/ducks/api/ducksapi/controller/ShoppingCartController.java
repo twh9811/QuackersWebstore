6ebc36ed6c1fd05bada23ac7d306bf275d02232b
Le <ajl5531@rit.edu>
2023-03-03 14:45:38 -0500

    implemented createShoppingCart function

--- estore-api/src/main/java/com/ducks/api/ducksapi/controller/ShoppingCartController.java
+++ estore-api/src/main/java/com/ducks/api/ducksapi/controller/ShoppingCartController.java
@@ -4,10 +4,17 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.boot.info.InfoProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ducks.api.ducksapi.model.Duck;
@@ -55,7 +62,7 @@ public class ShoppingCartController {
      */
     @GetMapping("")
     public ResponseEntity<ShoppingCart[]> getShoppingCarts(){
-        LOG.info("GET /ShoppingCart");
+        LOG.info("GET /shopping");
         try{
             ShoppingCart[] carts = cartDao.getShoppingCarts();
             if(carts != null) {
@@ -66,6 +73,25 @@ public class ShoppingCartController {
         } catch(IOException ioe) {
         LOG.log(Level.SEVERE, ioe.getLocalizedMessage());
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    
+    @PostMapping("")
+    public ResponseEntity<ShoppingCart> createShoppingCart(@RequestBody ShoppingCart cart) {
+        LOG.info("POST /shopping " + cart);
+        try{
+            ShoppingCart newCart = cartDao.createShoppingCart(cart);
+            if (newCart != null) {
+                return new ResponseEntity<ShoppingCart>(newCart,HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
 }
