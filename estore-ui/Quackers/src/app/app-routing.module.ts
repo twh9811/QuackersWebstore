34fb7283380a6ca8586c134c5eb14b35b8374f35
Le <ajl5531@rit.edu>
2023-03-13 04:14:56 -0400

    acidentally worked on main, reverted changes

--- estore-ui/Quackers/src/app/app-routing.module.ts
+++ estore-ui/Quackers/src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminTestComponent } from './admin-test/admin-test.component';
 import { CustomerTestComponent } from './customer-test/customer-test.component';
 import { LoginComponent } from './login/login.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/customerPage", pathMatch: "full"},
   { path: 'adminPage', component: AdminTestComponent },
   { path: 'customerPage', component: CustomerTestComponent },
   { path: "login", component : LoginComponent},
-  { path: 'shoppingCartPage', component: ShoppingCartComponent}
 ];
 
 @NgModule({
