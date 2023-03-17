c808695f8e7564c5d86e35e57ab55334e09265cd
bz5529 <bz5529@rit.edu>
2023-03-17 17:43:03 -0400

    implemented ducksearch function

--- estore-ui/Quackers/src/app/ducksearch/ducksearch.component.ts
+++ estore-ui/Quackers/src/app/ducksearch/ducksearch.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+
+import { Duck } from '../duck';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-ducksearch',
+  selector: 'app-duck-search',
   templateUrl: './ducksearch.component.html',
-  styleUrls: ['./ducksearch.component.css']
+  styleUrls: [ './ducksearch.component.css' ]
 })
-export class DucksearchComponent {
+export class DucksearchComponent implements OnInit {
+  ducks$!: Observable<Duck[]>;
+  private searchTerms = new Subject<string>();
 
+  constructor(private productService: ProductService) {}
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.ducks$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.productService.searchDuck(term)),
+    );
+  }
 }
