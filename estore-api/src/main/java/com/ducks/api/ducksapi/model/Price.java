0abda87aea731ec4129930ddde5bb100ccd4f26a
Travis <travishill62102@gmail.com>
2023-03-26 17:46:27 -0400

    Added JavaDocs for Price.java

--- estore-api/src/main/java/com/ducks/api/ducksapi/model/Price.java
+++ estore-api/src/main/java/com/ducks/api/ducksapi/model/Price.java
@@ -1,15 +1,30 @@
 package com.ducks.api.ducksapi.model;
 
+/**
+ * Represents the price of duck.
+ * 
+ * @author Travis Hill
+ */
 public class Price {
     private double finalPrice;
     private double basePrice;
     private double outfitPrice = 2.00;
-
-
+    
+    /*
+     * Creates a Price object that stores the price of a duck
+     * 
+     * @param duck The duck object the price is being made for.
+     */
     public Price(Duck duck) {
         this.finalPrice = calculatePrice(duck);
     }
 
+    /**
+     * Calculates the price of a duck based on its size, color, and outfit attributes.
+     * 
+     * @param duck The duck that the price is being based off of.
+     * @return The total price of the duck as a double.
+     */
     private double calculatePrice(Duck duck) {
         DuckOutfit outfit = duck.getOutfit();
         Colors color = duck.getColor();
@@ -27,6 +42,9 @@ public class Price {
         return this.basePrice;
     }
 
+    /*
+     * Returns the price of the price object.
+     */
     public double getPrice() {
         return this.finalPrice;
     }
