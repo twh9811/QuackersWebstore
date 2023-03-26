8a37eaba599c3a00d224f0556cfed2707d5abe72
Le <ajl5531@rit.edu>
2023-03-26 04:22:27 -0400

    still have problems with nav bar

--- estore-ui/Quackers/src/app/navigation-bar/navigation-bar.component.ts
+++ estore-ui/Quackers/src/app/navigation-bar/navigation-bar.component.ts
@@ -33,78 +33,33 @@ export class NavigationBarComponent {
   * Loads the ducks array when the page is opened
   */
   ngOnInit(): void {
-    if (!this.sessionService.session) {
-      this.validateAuthorization();
-      return;
-    }
 
     // Waits for account to be retrieved before doing anything else
     this.accountService.getAccount(this.sessionService.session.id).subscribe(account => {
       this._account = account;
-      this.validateAuthorization();
-      // Waits for the cart to load before loading the ducks
-      // This prevents issues such as the cart not loading
-      this.cartService.getCartAndCreate(this._account.id).then(cart => {
-        if (!cart) {
-          this.router.navigate(['/']);
-          this.notificationService.add("Unable to load your cart!", 3);
-          return;
-        }
-
-        this.cart = cart;
-        this.getDucks();
-      })
     });
-  }
-
-  /**
-   * Gets the ducks from the product service
-   */
-  getDucks(): void {
-    this.productService.getDucks().subscribe(ducks => this.ducks = ducks);
   }
 
   /**
   * Validates that a user is an customer
   * If not, they are sent back to the login page
-  */
+  
   private validateAuthorization(): void {
     if (this._account?.adminStatus || !this._account) {
       this.notificationService.add(`You are not authorized to view ${this.router.url}!`, 3);
       this.router.navigate(['/']);
     }
   }
+  */
 
-  /**
-   * Add a duck to shopping cart
-   * 
-   * @param duckId The id of the duck being added
-   */
-  addDuck(duckId: number): void {
-    this.productService.getDuck(duckId as number).subscribe(duck => {
-
-      if (!duck || duck.quantity < 1) {
-        this.notificationService.add(`Thee duck with the id of ${duckId} is no longer available!`, 3);
-        return;
-      }
-
-      this.cartService.addItem(this.cart!, duckId!, 1);
-      this.cartService.updateCart(this.cart!).subscribe(response => {
-        if (response.status == 200) {
-          this.notificationService.add(`Successfully added one duck with the id of ${duckId} to your cart!`, 3);
-          return;
-        }
-        this.notificationService.add(`Failed to add the duck with the id of ${duckId} to your cart!`, 3);
-      });
-    });
+  checkAdminStatus() : boolean {
+    if (this._account?.adminStatus || !this._account){
+      return true;
+    }
+    return false;
   }
 
-  /**
-   * Show details for a duck
-   * 
-   * @param duck The duck being showed
-   */
-  showDuck(id: number): void {
-    this.router.navigate([`/catalog/${id}`]);
+  logout() : void {
+    this.router.navigate([''])
   }
 }
