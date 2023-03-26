254864458c6882c80c01e86f19958a0cffe38497
Le <ajl5531@rit.edu>
2023-03-26 01:13:52 -0400

    created a image asset folder

---
+++ estore-ui/Quackers/src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.css']
+})
+export class NavigationBarComponent {
+  
+}
