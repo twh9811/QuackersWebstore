057066599a2fe8aa29f2704657cf49352e4c5139
Le <ajl5531@rit.edu>
2023-03-26 00:15:40 -0400

    Created Feature branch and component

--- estore-ui/Quackers/src/app/app.module.ts
+++ estore-ui/Quackers/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { DucksearchComponent } from './ducksearch/ducksearch.component';
 import { DuckDetailComponent } from './duck-detail/duck-detail.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { DuckDetailComponent } from './duck-detail/duck-detail.component';
     NotificationsComponent,
     CatalogComponent,
     DucksearchComponent,
-    DuckDetailComponent
+    DuckDetailComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
