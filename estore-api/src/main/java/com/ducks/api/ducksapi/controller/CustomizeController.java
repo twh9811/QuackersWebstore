7b4ceb5e8dbfe7558245f5701a2b40396d126a4d
bz5529 <bz5529@rit.edu>
2023-03-28 18:46:25 -0400

    created CustomizeController

---
+++ estore-api/src/main/java/com/ducks/api/ducksapi/controller/CustomizeController.java
@@ -0,0 +1,26 @@
+package com.ducks.api.ducksapi.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ducks.api.ducksapi.model.DuckOutfit;
+
+@RestController
+@RequestMapping("customize")
+public class CustomizeController {
+    private static final Logger LOG = Logger.getLogger(InventoryController.class.getName());
+     
+
+}
