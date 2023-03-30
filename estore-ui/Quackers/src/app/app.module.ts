6ea3b5dbd40222cb294063f0821a254b6f4970c6
Mason Bausenwein <theforbiddenai@gmail.com>
2023-03-30 12:42:26 -0400

    Greatly improved the design of the login page

--- estore-ui/Quackers/src/app/app.module.ts
+++ estore-ui/Quackers/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -46,7 +46,7 @@ import { ReceiptComponent } from './receipt/receipt.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatDatepickerModule,
+    MatSnackBarModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- estore-ui/Quackers/src/app/login/login.component.ts
+++ estore-ui/Quackers/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Account } from '../account';
@@ -14,26 +15,23 @@ import { SessionService } from '../session.service';
 })
 
 export class LoginComponent implements OnInit {
+  private _account: Account | undefined;
+
   message = "Please login to our store to continue :)";
-  feedback = "Fields cannot be empty, enter your information";
 
-  account: Account | undefined;
 
-  username: string = '';
-  password: string = '';
-
-  loginForm = this.formBuilder.group({
+  loginForm = this._formBuilder.group({
     username: '',
     password: ''
   });
 
-  constructor(private router: Router,
-    private accountService: AccountService,
-    private session: SessionService,
-    private formBuilder: FormBuilder) { }
+  constructor(private _router: Router,
+    private _accountService: AccountService,
+    private _session: SessionService,
+    private _formBuilder: FormBuilder, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
-    this.session.session = {
+    this._session.session = {
       type: "",
       username: "",
       plainPassword: "",
@@ -42,37 +40,43 @@ export class LoginComponent implements OnInit {
     };
   }
 
-  onSubmit(): void {
-
-  }
-
-  register(): void {
-    
-  }
-
+  /**
+   * Redirects the user to the correct page (catalog for buyers, inventory for admins)
+   */
   redirect(): void {
-    if (this.account) {
-      this.setSession(this.account);
-      if (this.account.username == "admin") {
-        this.router.navigate(['/inventory/'])
-      } else {
-        this.router.navigate(['/catalog']);
-      }
-    } else {
-      this.feedback = "Login failed, try again or register";
-    }
+    if (!this._account) return;
+
+    this.setSession(this._account);
+    this._router.navigate([this._account.username == "admin" ? '/inventory' : '/catalog'])
+    this.openSnackbar(`Welcome back ${this._account.username}!`);
   }
 
+  /**
+   * Handles login functionality
+   */
   onLogin(): void {
-    this.accountService.login(this.username, this.password).subscribe(account => {
-      this.account = account;
+    if (this.loginForm.invalid) return;
+
+    const controls = this.loginForm.controls;
+    this._accountService.login(controls.username.value!, controls.password.value!).subscribe(account => {
+      this._account = account;
+
+      if (!this._account) {
+        this.openSnackbar("You entered an invalid password or username.");
+        return;
+      }
+
       this.update();
       this.redirect();
     });
   }
 
+  /**
+   * Updates the session account storage
+   * @param account The account object of the user who just logged in
+   */
   setSession(account: Account): void {
-    this.session.session = {
+    this._session.session = {
       type: account.type,
       username: account.username,
       plainPassword: account.plainPassword,
@@ -81,10 +85,20 @@ export class LoginComponent implements OnInit {
     };
   }
 
-  onRegister(username: string, password: string): void {
+  /**
+   * Handles register functionality
+   */
+  onRegister(): void {
+    if (this.loginForm.invalid) {
+      this.markAllControlsAsTouched();
+      return;
+    }
+
+    const username: string = this.loginForm.controls.username.value!;
+    const password: string = this.loginForm.controls.password.value!;
     // Create account with placeholders for type, id, and admin status. These DONT matter.
     // These will be reformated with proper values in AccountFileDAO based on username.
-    this.account = {
+    this._account = {
       type: "UserAccount",
       id: -1,
       username: username,
@@ -92,30 +106,64 @@ export class LoginComponent implements OnInit {
       adminStatus: false
     };
 
-    this.accountService.createUser(this.account).subscribe(
-      account => {
-        if (account instanceof HttpErrorResponse) {
-          let cast: HttpErrorResponse = account;
-          var code = cast.status;
-          switch (code) {
-            case 406:
-              this.feedback = "Please make sure password has at least 8 characters and has 1 uppercase, 1 lowercase, and 1 number in it. Special characters are highly recommended but not required";
-              break;
-            case 409:
-              this.feedback = "Username already exists, select a different one";
-              break;
-          }
-        } else {
-          this.account = account;
-          this.update();
-          this.feedback = "Account created, please login to continue.";
-        }
-      });
+    this._accountService.createUser(this._account).subscribe(response => {
+      if (!(response instanceof HttpErrorResponse)) {
+        this._account = response;
+        this.update();
+        this.openSnackbar("Your account was successfully created. Please login to continue.");
+        return;
+      }
+
+      const httpResponse: HttpErrorResponse = response;
+      const code: number = httpResponse.status;
+      switch (code) {
+        case 406:
+          this.openSnackbar("Your password must be at least 8 characters long and have 1 uppercase letter, 1 lowercase letter, and 1 number.");
+          break;
+        case 409:
+          this.openSnackbar("An account with the name already exists. Please select a different one.");
+          break;
+      }
+
+    });
+  }
+
+  /**
+ * Handles password reset functionality
+ */
+  onPasswordReset(): void {
+    if (this.loginForm.invalid) {
+      this.markAllControlsAsTouched();
+      return;
+    }
+
+    this.openSnackbar("This is not implemented yet.");
   }
 
   // Updates the account variables (prevents double clicking buttons)
   update(): void {
-    console.log(this.account);
+    console.log(this._account);
+  }
+
+  /**
+   * Opens a snackbar that has a close button and lasts for 3 seconds with the given message
+   * @param message The message to display
+   */
+  private openSnackbar(message: string): void {
+    this.snackbar.open(message, "Close", { duration: 3000 })
+  }
+
+  /**
+   * Marks all controls as touched to allow their errors to be displayed if they aren't already
+   */
+  private markAllControlsAsTouched(): void {
+    const controls = this.loginForm.controls;
+
+    // Sets the type of name to the type of the attributes in <controls>
+    let name: keyof typeof controls;
+    for (name in controls) {
+      controls[name].markAsTouched();
+    }
   }
 
 }
