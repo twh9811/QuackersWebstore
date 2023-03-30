949de47a7c3a2144576d6b8d06dcebf255105d89
bz5529 <bz5529@rit.edu>
2023-03-30 19:14:09 -0400

    command

--- estore-api/src/main/java/com/ducks/api/ducksapi/controller/CustomizeController.java
+++ estore-api/src/main/java/com/ducks/api/ducksapi/controller/CustomizeController.java
@@ -30,35 +30,35 @@ public class CustomizeController {
         this.duckDao = duckDao;
     }
 
-    @GetMapping("/customize/{id}")
-    public ResponseEntity<Duck> getDuckOutfit()(@PathVariable int id) {
-        LOG.info("GET /customize/" + id);
-        try {
-            DuckOutfit duckOutfit = duckDao.getDuck(id).duckOutfit;
-            if (duckOutfit != null) {
-                return new ResponseEntity<DuckOutfit>(duckOutfit, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        } catch (IOException e) {
-            LOG.log(Level.SEVERE, e.getLocalizedMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    // @GetMapping("/customize/{id}")
+    // public ResponseEntity<Duck> getDuckOutfit()(@PathVariable int id) {
+    //     LOG.info("GET /customize/" + id);
+    //     try {
+    //         DuckOutfit duckOutfit = duckDao.getDuck(id).duckOutfit;
+    //         if (duckOutfit != null) {
+    //             return new ResponseEntity<DuckOutfit>(duckOutfit, HttpStatus.OK);
+    //         } else {
+    //             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    //         }
+    //     } catch (IOException e) {
+    //         LOG.log(Level.SEVERE, e.getLocalizedMessage());
+    //         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    //     }
+    // }
 
-    @GetMapping("")
-    public ResponseEntity<Duck[]> getDucksOutfit() {
-        LOG.info("GET /customize");
-        try {
-            Duck[] ducks = duckDao.getDucks();
-            if (ducks != null && ducks.length != 0) {
-                return new ResponseEntity<Duck[]>(ducks, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-        } catch (IOException ioe) {
-            LOG.log(Level.SEVERE, ioe.getLocalizedMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    // @GetMapping("")
+    // public ResponseEntity<Duck[]> getDucksOutfit() {
+    //     LOG.info("GET /customize");
+    //     try {
+    //         Duck[] ducks = duckDao.getDucks();
+    //         if (ducks != null && ducks.length != 0) {
+    //             return new ResponseEntity<Duck[]>(ducks, HttpStatus.OK);
+    //         } else {
+    //             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    //         }
+    //     } catch (IOException ioe) {
+    //         LOG.log(Level.SEVERE, ioe.getLocalizedMessage());
+    //         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    //     }
+    // }
 }
