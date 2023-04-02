eb51dda36d25c189963d184000c26d194b7c41b5
Mason Bausenwein <theforbiddenai@gmail.com>
2023-04-02 12:07:30 -0400

    Made some slight style changes

--- estore-ui/Quackers/src/app/app.component.ts
+++ estore-ui/Quackers/src/app/app.component.ts
@@ -7,20 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quacker\'s Duck Emporium';
 
   // router needs to be public for angular to compile (used in app.component.html)
   constructor(public router: Router) {}
 
-  goToAccount() : void {
-    this.router.navigate(['/account'])
-  }
-  /**
-  * If called, the site will logout the user and
-  * redirect the user back to the login page
-  * 
-  */
-  logout() : void {
-    this.router.navigate([''])
-  }
 }
