fdbde3e6e3bf321d65784bbce465abc7cc4499b8
Le <ajl5531@rit.edu>
2023-04-01 22:53:31 -0400

    got rid of duplicate imports

--- estore-ui/Quackers/src/app/app-routing.module.ts
+++ estore-ui/Quackers/src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { DuckDetailComponent } from './duck-detail/duck-detail.component';
 import { InventoryManagementComponent } from './inventory-management/inventory-management.component';
 import { LoginComponent } from './login/login.component';
 import { ProductCreateComponent } from './product-create-modify/product-create-modify.component';
-import { CatalogComponent } from './catalog/catalog.component';
-import { DuckDetailComponent } from './duck-detail/duck-detail.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CustomizeComponent } from './customize/customize.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
